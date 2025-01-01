c60cd5a1b521a57ea672f8eaac936c7d39a438db
Acrazie <mayeul.desbazeille@epitech.eu>
2025-01-01 18:20:02 +0100

    adjust display Signout button

--- authspeedrun/app/page.tsx
+++ authspeedrun/app/page.tsx
@@ -79,12 +79,14 @@ export default function Home() {
       <div className="flex justify-center items-center h-screen flex-col gap-2">
         <h1 className="text-8xl">Hello Homepage</h1>
         {session && <p className="mb-4">Bienvenue {session?.user?.name}</p>}
-        <button
-          onClick={() => signOut()}
-          className="px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
-        >
-          SignOut
-        </button>
+        {session && (
+          <button
+            onClick={() => signOut()}
+            className="px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
+          >
+            SignOut
+          </button>
+        )}
       </div>
     </>
   );
