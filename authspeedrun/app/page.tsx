bdfb105441614d9947a18de4b176620f04067111
Acrazie <mayeul.desbazeille@epitech.eu>
2025-01-01 19:00:54 +0100

    adding env variables

--- authspeedrun/app/page.tsx
+++ authspeedrun/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
             href="#"
             className="text-gray-800 transition-colors duration-300 transform dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6"
           >
-            home
+            Home
           </a>
 
           <a
